7982467255791bc693bcbe980d78cf92d7000291
jinyuttt <jinyu20072787@sina.com>
2019-01-23 16:33:08 +0800

    Signed-off-by: jinyuttt <jinyu20072787@sina.com>

--- Hikari/obj/Debug/netstandard2.0/Hikari.AssemblyInfo.cs
+++ Hikari/obj/Debug/netstandard2.0/Hikari.AssemblyInfo.cs
@@ -15,11 +15,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("2019")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("c#数据库连接池   个人贡献，任意使用，希望保留版本和作者注释")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("2.1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.1.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hikari")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hikari")]
-[assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("2.1.0.0")]
 
 // 由 MSBuild WriteCodeFragment 类生成。
 
